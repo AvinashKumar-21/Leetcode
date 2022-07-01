8ac16c29c5e5ac394be6af51896997bfccc73c64
AvinashKumar-21 <107938394+AvinashKumar-21@users.noreply.github.com>
2022-07-01 10:33:47 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,113 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        stack<Node*>s;
+        s.push(head);
+        head=head->next;
+        while(head)
+        {
+            Node*a=head->next;
+            head->next=NULL;
+            while(!s.empty() && s.top()->data < head->data)
+            s.pop();
+            s.push(head);
+            head=a;
+        }
+        Node*temp=NULL;
+        while(!s.empty())
+        {
+            if(temp==NULL)
+            temp=s.top();
+            else
+            {
+                s.top()->next=temp;
+                temp=s.top();
+            }
+            s.pop();
+        }
+        return temp;
+    }
+    
+};
+   
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+  // } Driver Code Ends
