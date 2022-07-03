4c20ee958ba9661882161dd964a300e1b0dbd7b3
AvinashKumar-21 <107938394+AvinashKumar-21@users.noreply.github.com>
2022-07-03 11:26:21 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    int c=0;
+    int a=1;
+    void fun(Node*head)
+    {
+        if(!head)
+        return ;
+        addOne(head->next);
+        head->data=head->data+a+c;
+        c=head->data/10;
+        head->data=head->data%10;
+        a=0;
+    }
+    Node* addOne(Node *head) 
+    {
+        fun(head);
+        if(c!=0)
+        {
+            Node*temp=new Node(c);
+            temp->next=head;
+            return temp;
+        }
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
