2166acc1d69f3b29dfcb793c582f3bcc8b1e8988
AvinashKumar-21 <107938394+AvinashKumar-21@users.noreply.github.com>
2022-07-10 01:05:04 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/* Structure of the linked list node is as
+struct Node 
+{
+    int data;
+    struct Node* next;
+    Node(int x) { data = x;  next = NULL; }
+};
+*/
+
+
+class Solution{
+  public:
+    //Function to sort the given linked list using Merge Sort.
+    Node*merge(Node*l1,Node*l2)
+    {
+        if(!l1)
+        return l2;
+        if(!l2)
+        return l1;
+        Node*temp=new Node(-1);
+        Node*trail=temp;
+        while(l1 && l2)
+        {
+            if(l1->data>l2->data)
+            {
+                trail->next=l2;
+                trail=l2;
+                l2=l2->next;
+            }
+            else
+            {
+                trail->next=l1;
+                trail=l1;
+                l1=l1->next;
+            }
+        }
+        if(l1)
+        trail->next=l1;
+        if(l2)
+        trail->next=l2;
+        return temp->next;
+    }
+    Node*mid(Node*head)
+    {
+        Node*fast=head,*slow=head;
+        while(fast->next && fast->next->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        Node*temp=slow->next;
+        slow->next=NULL;
+        return temp;
+    }
+    Node* mergeSort(Node* head) 
+    { 
+        if(!head || !head->next)
+        return head;
+        Node*m=mid(head);
+        return merge(mergeSort(head),mergeSort(m));
+    }
+};
+
+
+// { Driver Code Starts.
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    long test;
+    cin >> test;
+    while (test--) {
+        struct Node* a = NULL;
+        long n, tmp;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> tmp;
+            push(&a, tmp);
+        }
+        Solution obj;
+        a = obj.mergeSort(a);
+        printList(a);
+    }
+    return 0;
+}  // } Driver Code Ends
