b18b1e144eb75c8ea2105391e182f916fb65e936
AvinashKumar-21 <107938394+AvinashKumar-21@users.noreply.github.com>
2022-07-15 15:52:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.55%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,10 +3,8 @@ public:
     bool isPowerOfTwo(int n) 
     {
         long long temp1=n;
-        long long temp=temp1-1;
-        
         if(n==0)
             return false;
-        return (temp1 & temp)==0;
+        return (temp1 & temp1-1)==0;
     }
 };
