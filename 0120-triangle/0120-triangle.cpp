6fdde0baa7c8ac89af24071de7a54bf2dd78bc41
AvinashKumar-21 <107938394+AvinashKumar-21@users.noreply.github.com>
2022-11-28 16:47:52 +0530

    Time: 7 ms (88.60%), Space: 8.6 MB (93.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) 
     {
-        int n=t.size();
-        for(int i=n-2;i>=0;i--)
+        int n=t.size()-1;
+        for(int i=n-1;i>=0;i--)
         {
             for(int j=0;j<t[i].size();j++)
-            {
                 t[i][j]+=min(t[i+1][j],t[i+1][j+1]);
-            }
         }
-        // for(auto i:t)
-        // {
-        //     for(auto j:i)
-        //         cout<<j<<" ";
-        //     cout<<"\n";
-        // }    
         return t[0][0];
     }
 };
